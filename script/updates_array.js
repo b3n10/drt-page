840fa03ece6b311702db6ce7212be075643602ec
b3n10 <francisben.lleve@gmail.com>
2017-04-30 16:06:48 +0800

    Added about page

--- script/updates_array.js
+++ script/updates_array.js
@@ -2,15 +2,24 @@ var updates = {
     'Manager-apr282017': {
         'title': 'Manager List',
         'date': 'Apr 28, 2017',
+        'link': 'http://www.link.com',
     },
     'Checklist-apr272017': {
         'title': 'Checklist',
         'date': 'Apr 27, 2017',
+        'link': 'http://www.link.com',
     },
     'Somelist-apr252017': {
         'title': 'Some List',
         'date': 'Apr 25, 2017',
+        'link': 'http://www.link.com',
     },
+};
+
+var links = {
+    'Link1-Title': {'link': 'www.link.com'},
+    'Link2-Title': {'link': 'www.link.com'},
+    'Link3-Title': {'link': 'www.link.com'},
 };
 
 //script from https://www.creativejuiz.fr/blog/en/javascript-en/read-url-get-parameters-with-javascript
